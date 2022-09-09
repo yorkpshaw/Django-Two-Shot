fa1ef8765b5e0e6d9070afa9cb174380ae9cbd7e
York <york.p.shaw@gmail.com>
2022-09-09 15:34:25 -0500

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from accounts.views import signup
+
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", signup, name="signup"),
 ]
