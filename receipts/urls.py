00c806ceb580a1522f6ed9e2968313411284580e
York <york.p.shaw@gmail.com>
2022-09-09 16:16:38 -0500

    Feature 11 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 from receipts.views import (
     ReceiptListView,
+    ReceiptCreateView,
 )
 
 urlpatterns = [
     path("", ReceiptListView.as_view(), name="home"),
+    path("create/", ReceiptCreateView.as_view(), name="create_receipt"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from receipts.models import Receipt
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 
 
 class ReceiptListView(LoginRequiredMixin, ListView):
@@ -11,3 +13,15 @@ class ReceiptListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Receipt.objects.filter(purchaser=self.request.user)
+
+
+class ReceiptCreateView(LoginRequiredMixin, CreateView):
+    model = Receipt
+    template_name = "receipts/create.html"
+    fields = ["vendor", "total", "tax", "date", "category", "account"]
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.purchaser = self.request.user
+        item.save()
+        return redirect("home")
